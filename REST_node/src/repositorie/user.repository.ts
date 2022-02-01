81270c18b1e124cb8cc519fddf115372ccc23dbb
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2022-01-31 23:07:48 -0300

    Funções do CRUD com conexão com o banco de dados

---
+++ REST_node/src/repositorie/user.repository.ts
@@ -0,0 +1,99 @@
+import db from "../config/db";
+import DataBaseError from "../models/errors/database.error.model";
+import User from "../models/user.model";
+
+class UserRepository {
+  
+  //? GET ALL USERS
+  async findAllUsers(): Promise<User[]> {
+    const query = `
+    SELECT 
+    uuid, username
+    FROM 
+    application_user;
+    `;
+    
+    const { rows } = await db.query<User[]>(query);
+    return rows || [];
+  }
+  
+  //? GET SPECIFIC USER
+  async findUserById(uuid: string): Promise<User> {
+    try {
+      const query = `
+      SELECT 
+        uuid, username
+      FROM
+        application_user
+      WHERE
+        uuid = $1
+      `;
+      const values = [uuid];
+      
+      const { rows } = await db.query(query, values);
+      const user = rows;
+      
+      return user[0];
+    }
+    catch (err) {
+      console.log(err);
+      throw new DataBaseError("Erro na consulta por ID", err);
+    }
+  }
+  
+  //? CREATE USER
+  async createUser(user: User): Promise<string> {
+    const { username, password } = user;
+    
+    const query = `
+    INSERT INTO
+    application_user (username, password)
+    VALUES
+    ($1, crypt($2, 'my_salt'))
+    RETURNING
+    uuid
+    `;
+    
+    const values =  [username, password];
+    
+    const { rows } = await db.query<{ uuid: string }>(query, values);
+    const [ newUser ] = rows;
+    
+    return newUser.uuid;
+  }
+  
+  //? UPDATE USER
+  async updateUser(user: User): Promise<void> {
+    const { uuid, username, password } = user;
+    
+    const query = `
+    UPDATE
+    application_user
+    SET
+    username = $1, 
+    password = crypt($2, 'my_salt')
+    WHERE uuid = $3
+    `;
+    
+    const values =  [username, password, uuid];
+    
+    await db.query(query, values);
+  }
+  
+  //? DELETE USER
+  async deleteUser(uuid: string): Promise<void> {
+    const query = `
+    DELETE FROM
+    application_user
+    WHERE
+    uuid = $1
+    `;
+    
+    const values = [uuid];
+    
+    await db.query(query, values);
+  }
+  
+}
+
+export default new UserRepository();
